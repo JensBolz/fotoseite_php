70804fd9d04212ad86f1b9333fe724d6291d4ee2
Jens Bolz <jens.bolz@gameforge.com>
2017-09-28 14:27:00 +0200

    Impressum eingefügt

---
+++ imprint.php
@@ -0,0 +1,199 @@
+<!DOCTYPE html>
+<html lang="de">
+   <head>
+        <title>Home</title>
+        <?php
+        include './head.php';
+        ?>
+    </head>
+    <body class="" id="top">
+        <!--==============================header=================================-->
+        <header>
+            <div id="stuck_container">
+                <div class="container">
+                    <div class="row">
+                        <div class="grid_12">
+                            <div class="logo">Jens Bolz Photography</div>
+                            <div class="menu_block ">
+                                <nav class="horizontal-nav full-width horizontalNav-notprocessed">
+                                    <ul class="sf-menu">
+                                        <li><a href="index.php">Über</a></li>
+                                        <li class="current">Impressum</li>
+                                        <li><a href="contact.php">Kontakt</a></li>
+
+                                    </ul>
+                                </nav>
+                                <div class="clear"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
+        <div class="main">
+            <!--=====================Content======================-->
+            <section class="content"> 
+                <div class="container">
+                    <div class="adress">
+                        <div class="highlighted"><span style="font-size: 13px;">Angaben gemäß § 5 TMG:</span></div>             
+
+                        <p>Jens Bolz | Pommernstraße 7 | 76646 Bruchsal</p>
+                        <p>mobil: (+49) 172 - 625 90 44</p>
+                        <p>eMail: <a title="foto@jens-bolz.de" href="mailto:foto@jens-bolz.de">foto@jens-bolz.de</a></p>
+                        <p>Steuernummer: 31214/31774</p>
+                    </div>
+                    <hr/>
+                    <div class="highlighted">Keine Abmahnung ohne vorherigen Kontakt</div>
+                    <div class="paragraph">
+                        <p>
+                            Sollte der Inhalt oder die
+                            Aufmachung dieser Seiten fremde Rechte Dritter oder gesetzliche Bestimmungen verletzen, so bitte ich um eine entsprechende Nachricht ohne Kostennote. Ich garantiere, dass die zu Recht
+                            beanstandeten Passagen unverzüglich entfernt werden, ohne dass von Ihrer Seite die Einschaltung eines Rechtsbeistandes erforderlich ist. Dennoch von Ihnen ohne vorherige Kontaktaufnahme
+                            ausgelöste Kosten werde ich vollumfänglich zurückweisen und gegebenenfalls Gegenklage wegen Verletzung vorgenannter Bestimmungen einreichen.
+                        </p>
+                    </div>                
+                    <hr/>
+                    <div class="highlighted">Haftungsausschluss (Disclaimer):</div>                 
+                    <div class="headline">Haftung für Inhalte</div>
+                    <div class="paragraph">
+                        <p>
+                            Als Diensteanbieter bin ich gemäß § 7 Abs.1 TMG für eigene Inhalte auf diesen Seiten nach den allgemeinen Gesetzen verantwortlich. Nach §§ 8 bis 10 TMG bin ich als Diensteanbieter jedoch nicht
+                            verpflichtet, übermittelte oder gespeicherte fremde Informationen zu überwachen oder nach Umständen zu forschen, die auf eine rechtswidrige Tätigkeit hinweisen. Verpflichtungen zur Entfernung
+                            oder Sperrung der Nutzung von Informationen nach den allgemeinen Gesetzen bleiben hiervon unberührt. Eine diesbezügliche Haftung ist jedoch erst ab dem Zeitpunkt der Kenntnis einer konkreten
+                            Rechtsverletzung möglich. Bei Bekanntwerden von entsprechenden Rechtsverletzungen werde ich diese Inhalte umgehend entfernen.
+                        </p>
+                    </div>                    
+                    <div class="headline">Haftung für Links</div>
+                    <div class="paragraph">
+                        Mein Angebot enthält Links zu externen Webseiten Dritter, auf deren Inhalte ich keinen Einfluss habe. Deshalb kann ich für diese fremden Inhalte auch keine Gewähr übernehmen. Für die Inhalte der
+                        verlinkten Seiten ist stets der jeweilige Anbieter oder Betreiber der Seiten verantwortlich. Die verlinkten Seiten wurden zum Zeitpunkt der Verlinkung auf mögliche Rechtsverstöße überprüft.
+                        Rechtswidrige Inhalte waren zum Zeitpunkt der Verlinkung nicht erkennbar. Eine permanente inhaltliche Kontrolle der verlinkten Seiten ist jedoch ohne konkrete Anhaltspunkte einer Rechtsverletzung
+                        nicht zumutbar. Bei Bekanntwerden von Rechtsverletzungen werde ich derartige Links umgehend entfernen.
+                    </div>          
+                    <div class="headline">Urheberrecht</div>
+                    <div class="paragraph">
+                        Die durch die Seitenbetreiber erstellten Inhalte und Werke auf diesen Seiten unterliegen dem deutschen Urheberrecht. Die Vervielfältigung, Bearbeitung, Verbreitung und jede Art der Verwertung
+                        außerhalb der Grenzen des Urheberrechtes bedürfen der schriftlichen Zustimmung des jeweiligen Autors bzw. Erstellers. Downloads und Kopien dieser Seite sind nicht gestattet. Soweit die Inhalte
+                        auf dieser Seite nicht vom Betreiber erstellt wurden, werden die Urheberrechte Dritter beachtet. Insbesondere werden Inhalte Dritter als solche gekennzeichnet. Sollten Sie trotzdem auf eine
+                        Urheberrechtsverletzung aufmerksam werden, bitte ich um einen entsprechenden Hinweis. Bei Bekanntwerden von Rechtsverletzungen werde ich derartige Inhalte umgehend entfernen.
+                    </div>
+                    <hr/>
+                    <div class="highlighted">Datenschutzerklärung:</div> 
+                    <div class="headline">Datenschutz</div> 
+                    <div class="paragraph">
+                        <p>Die Nutzung meiner Webseite ist in der Regel ohne Angabe personenbezogener Daten möglich. Soweit auf meinen Seiten personenbezogene Daten (beispielsweise Name, Anschrift oder eMail-Adresse)
+                            erhoben werden, erfolgt dies, soweit möglich, stets auf freiwilliger Basis. Diese Daten werden ohne Ihre ausdrückliche Zustimmung nicht an Dritte weitergegeben.</p>
+                        <p>
+                            Ich weise darauf hin, dass die Datenübertragung im Internet (z.B. bei der Kommunikation per eMail) Sicherheitslücken aufweisen kann. Ein lückenloser Schutz der Daten vor dem Zugriff durch Dritte
+                            ist nicht möglich.
+                        </p>
+                        <p>
+                            Der Nutzung von im Rahmen der Impressumspflicht veröffentlichten Kontaktdaten durch Dritte zur Übersendung von nicht ausdrücklich angeforderter Werbung und Informationsmaterialien wird hiermit
+                            ausdrücklich widersprochen. Ich behalten mir ausdrücklich rechtliche Schritte im Falle der unverlangten Zusendung von Werbeinformationen, etwa durch Spam-Mails, vor.
+                        </p>
+                    </div>  
+                    <div class="headline">Datenschutzerklärung für die Nutzung von Facebook-Plugins (Like-Button)</div>  
+                    <div class="paragraph">
+                        <p>
+                            Auf meinen Seiten sind Plugins des sozialen Netzwerks Facebook (Facebook Inc., 1601 Willow Road, Menlo Park, California, 94025, USA) integriert. Die Facebook-Plugins erkennen Sie an dem
+                            Facebook-Logo oder dem “Like-Button” (“Gefällt mir”) auf meinen Seiten. Eine Übersicht über die Facebook-Plugins finden Sie hier: <a href="http://developers.facebook.com/docs/plugins/" target="_blank" data-mce-href="http://developers.facebook.com/docs/plugins/">http://developers.facebook.com/docs/plugins/</a>.<br/>
+                            Wenn Sie meine Seiten besuchen, wird über das Plugin eine direkte Verbindung zwischen Ihrem Browser und dem Facebook-Server hergestellt. Facebook erhält dadurch die Information, dass Sie mit
+                            Ihrer IP-Adresse unsere Seite besucht haben. Wenn Sie den Facebook “Like-Button” anklicken während Sie in Ihrem Facebook-Account eingeloggt sind, können Sie die Inhalte meiner Seiten auf Ihrem
+                            Facebook-Profil verlinken. Dadurch kann Facebook den Besuch meiner Seiten Ihrem Benutzerkonto zuordnen. Ich weise darauf hin, dass ich als Anbieter der Seiten keine Kenntnis vom Inhalt der
+                            übermittelten Daten sowie deren Nutzung durch Facebook erhalte. Weitere Informationen hierzu finden Sie in der Datenschutzerklärung von facebook unter <a href="http://de-de.facebook.com/policy.php" target="_blank" data-mce-href="http://de-de.facebook.com/policy.php">http://de-de.facebook.com/policy.php</a><br data-mce-bogus="1"/>
+                        </p>
+
+
+
+                        <p>
+                            Wenn Sie nicht wünschen, dass Facebook den Besuch meiner Seiten Ihrem Facebook-Nutzerkonto zuordnen kann, loggen Sie sich bitte aus Ihrem Facebook-Benutzerkonto aus.
+                        </p>
+                    </div>
+
+                    <div class="headline">Datenschutzerklärung für die Nutzung von Google Analytics</div>
+                    <div class="paragraph">
+                        <p>
+                            Diese Webseite benutzt Google Analytics, einen Webanalysedienst der Google Inc. (“Google”). Google Analytics verwendet sog. “Cookies”, Textdateien, die auf Ihrem Computer gespeichert werden und
+                            die eine Analyse der Benutzung der Webseite durch Sie ermöglichen. Die durch den Cookie erzeugten Informationen über Ihre Benutzung dieser Webseite werden in der Regel an einen Server von Google
+                            in den USA übertragen und dort gespeichert. Im Falle der Aktivierung der IP-Anonymisierung auf dieser Webseite wird Ihre IP-Adresse von Google jedoch innerhalb von Mitgliedstaaten der
+                            Europäischen Union oder in anderen Vertragsstaaten des Abkommens über den Europäischen Wirtschaftsraum zuvor gekürzt.
+                        </p>
+                        <p>
+                            Nur in Ausnahmefällen wird die volle IP-Adresse an einen Server von Google in den USA übertragen und dort gekürzt. Im Auftrag des Betreibers dieser Webseite wird Google diese Informationen
+                            benutzen, um Ihre Nutzung der Webseite auszuwerten, um Reports über die Webseitenaktivitäten zusammenzustellen und um weitere mit der Webseitennutzung und der Internetnutzung verbundene
+                            Dienstleistungen gegenüber dem Webseitenbetreiber zu erbringen. Die im Rahmen von Google Analytics von Ihrem Browser übermittelten IP-Adresse wird nicht mit anderen Daten von Google
+                            zusammengeführt.
+                        </p>
+                        <p>
+                            Sie können die Speicherung der Cookies durch eine entsprechende Einstellung Ihrer Browser-Software verhindern. Ich weise Sie jedoch darauf hin, dass Sie in diesem Fall gegebenenfalls nicht
+                            sämtliche Funktionen dieser Webseite vollumfänglich werden nutzen können. Sie können darüber hinaus die Erfassung der durch das Cookie erzeugten und auf Ihre Nutzung der Webseite bezogenen Daten
+                            (inkl. Ihrer IP-Adresse) an Google, sowie die Verarbeitung dieser Daten durch Google verhindern, indem Sie das unter dem folgenden Link verfügbare Browser-Plugin herunterladen und installieren:
+                            <a target="_top" href="http://tools.google.com/dlpage/gaoptout?hl=de" data-mce-href="http://tools.google.com/dlpage/gaoptout?hl=de">http://tools.google.com/dlpage/gaoptout?hl=de</a>.
+                        </p>
+                    </div>           
+                    <div class="headline">Auskunft, Löschung, Sperrung</div>
+                    <div class="paragraph">
+                        Sie haben jederzeit das Recht auf unentgeltliche Auskunft über Ihre gespeicherten personenbezogenen Daten, deren Herkunft und Empfänger und den Zweck der Datenverarbeitung, sowie ein Recht auf
+                        Berichtigung, Sperrung oder Löschung dieser Daten. Hierzu, sowie zu weiteren Fragen zum Thema personenbezogene Daten, können Sie sich jederzeit über die im Impressum angegebene Adresse an mich
+                        wenden.
+                    </div> <div class="copyright">
+                        <p>Alle gezeigten Photos und sämtliche Inhalte dieser Webseite unterliegen dem Urheberrecht.</p>
+                        <p>Die Nutzung, Vervielfältigung oder der Vertrieb sind ohne schriftliche Genehmigung untersagt.</p>
+                        <p>Zuwiderhandlungen werden zur Anzeige gebracht.</p>
+                    </div>
+            </section>       
+            <!--==============================Bot_block=================================-->
+<!--            <section class="bottom_block">
+                <div class="container">
+                    <div class="row">
+                        <div class="grid_5">
+                            <div class="bot_title">
+                                <span class="fa fa-envelope-o"></span>Subscribe to My Newsletter
+                            </div>
+                        </div>
+                        <div class="grid_7">
+                            <form id="newsletter">
+                                <div class="row">
+                                    <div class="grid_4">
+                                        <div class="rel">
+                                            <div class="success">Your subscrsibe request has been sent!</div>
+                                            <label class="email">
+                                                <input type="email" value="Your Email Address Here" >
+                                                <span class="error">*This is not a valid email address.</span>
+                                                <span class="clear"></span>
+                                            </label>
+                                        </div>
+                                    </div>
+                                    <div class="grid_3">
+                                        <a href="#" class="btn bt__2" data-type="submit">Submit</a>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </section>-->
+            <!--==============================footer=================================-->
+            <footer>
+                <div class="container">
+                    <div class="row">
+                        <div class="grid_12">
+<!--                            <div class="footer_socials">
+                                <a href="#" class="fa fa-twitter"></a>
+                                <a href="#" class="fa fa-facebook"></a>
+                                <a href="#" class="fa fa-google-plus"></a>
+                                <a href="#" class="fa fa-pinterest"></a>
+                            </div>-->
+                            <div class="copy">
+                                <span class="brand">Jens Bolz</span> <script type="text/javascript" language="JavaScript">
+                                    DatumAktuell = new Date();
+                                    JahrAktuell = DatumAktuell.getFullYear();
+                                    document.write("&copy; 2009 - " + JahrAktuell);
+                                </script> | <a>Impressum</a>                                
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </footer>
+    </body>
+</html>
