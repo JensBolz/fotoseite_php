259ada6fbeac90f1f68d92c3bac23ebdd4f73461
Jens Bolz <jens.bolz@gameforge.com>
2017-09-28 12:16:31 +0200

    Footer Links deaktiviert

--- footer.php
+++ footer.php
@@ -1,12 +1,12 @@
 <div class="container">
     <div class="row">
         <div class="grid_12">
-            <div class="footer_socials">
+<!--            <div class="footer_socials">
                 <a href="#" class="fa fa-twitter"></a>
                 <a href="#" class="fa fa-facebook"></a>
                 <a href="#" class="fa fa-google-plus"></a>
                 <a href="#" class="fa fa-pinterest"></a>
-            </div>
+            </div>-->
             <div class="copy">
                 <span class="brand">Jens Bolz</span> &copy; <span id="copyright-year"></span> | <a href="imprint.html">Impressum</a>                             
             </div>
