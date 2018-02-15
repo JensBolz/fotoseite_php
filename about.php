e80c0fbacb475edbfac5b2507cbc462dae0b5363
Jens Bolz <jens.bolz@gameforge.com>
2018-02-15 11:12:16 +0100

    new about site

---
+++ about.php
@@ -0,0 +1,157 @@
+<!DOCTYPE html>
+<html lang="de">
+    <head>
+        <title>Über mich</title>
+        <?php
+        include './head.php';
+        ?>
+    </head>
+    <body class="page1" id="top" oncontextmenu="return true">
+        <!--==============================header=================================-->
+        <header>
+            <div id="stuck_container">
+                <div class="container">
+                    <div class="row">
+                        <div class="grid_12">
+                            <h1>
+                                <a href="index.php">
+                                    <img src="images/logo.png" alt="Your Happy Family">
+                                </a>
+                            </h1>
+                            <div class="menu_block">
+                                <nav class="horizontal-nav full-width horizontalNav-notprocessed">
+                                    <ul class="sf-menu">
+                                        <li><a href="index.php">Start</a></li>
+                                        <li><a href="portfolio_main.php">Portfolio</a></li>
+                                        <li class="current">Über mich</li>
+                                        <li> <a href="contact.php">Kontakt</a></li>                                        
+
+                                    </ul>
+                                </nav>
+                                <div class="clear"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>   
+
+        </header>
+        <div class="main">
+            <!--=====================Content======================-->
+            <div class="block1">
+                <div class="container">
+                    <div class="row">
+                        <div class="grid_12">
+                            <div class="rel">
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>     
+            <section>
+                <div class="container">
+                    <div class="row">
+                        <div class="grid_12">                               
+
+
+                            <div class="row">
+                                <div class="clear"></div>
+                                <div class="grid_4a">
+                                    <h3>Über mich</h3>
+                                    <p>
+                                        Als hauptberuflicher Software- und Webentwickler ist Fotografie für mich ein Ausgleich zum Beruf und eine Art Meditation. Bei meinen eigenen Projekten oder Fototouren fotografiere das was mir Spaß macht und folge nicht einem bestimmten Trend oder irgendwelchen Vorgaben.<br>
+
+                                        Ich probiere auch gerne das ein oder andere aus wie z.B. Blitztechniken mit Aufsteckblitzen oder neue Wege in der Bildbarbeitung.
+                                    </p>
+                                    <p></p>
+                                </div>
+                            </div> 
+
+                            <div class="row">
+                                <div class="grid_4a">
+                                    <h3>Fotos im Netz</h3>
+                                    <ul class="list1">
+                                        <li>
+                                            <div class="list_count">1</div>
+                                            <div class="extra_wrapper">
+                                                <div class="list1_title color2"><a href="https://500px.com/jens79" target="_blank">500px </a></div>Meine Fotos auf 500px
+                                                <a href="https://500px.com/jens79" target="_blank" class="color1 td_und">More</a>
+                                            </div>
+                                        </li>
+                                        <li>
+                                            <div class="list_count">2</div>
+                                            <div class="extra_wrapper">
+                                                <div class="list1_title color2"><a href="https://www.flickr.com/photos/jensbolz/" target="_blank">FLICKR</a></div>Meine Fotos auf FLICKR
+                                                <a href="https://www.flickr.com/photos/jensbolz/" target="_blank" class="color1 td_und">More</a>
+                                            </div>
+                                        </li> 
+                                        <li>
+                                            <div class="list_count">3</div>
+                                            <div class="extra_wrapper">
+                                                <div class="list1_title color2"><a href="http://www.fotocommunity.de/fotograf/jens-bolz/2147626" target="_blank">fotocommunity</a></div>Meine Fotos in der fotocommunity
+                                                <a href="http://www.fotocommunity.de/fotograf/jens-bolz/2147626" target="_blank" class="color1 td_und">More</a>
+                                            </div>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+
+                            <div class="row">
+                                <div class="clear"></div>
+                                <div class="grid_4a">
+                                    <h3 class="mb__0">Equipment</h3>
+                                    <div class="grid_4">
+                                        <div class="block2_title color1">Kameras</div>
+                                        <ul class="list">
+                                            <li>Nikon D3100</li>
+                                            <li>Nikon D7100</li>
+                                            <li>Nikon D7100 mit MB-D15</li>                                                    
+                                        </ul>
+                                    </div>
+                                    <div class="grid_4">
+                                        <div class="block2_title color1">Objektive</div>
+                                        <ul class="list">                                                  
+                                            <li>Nikon AF-S 35mm 1.8 DX</li>
+                                            <li>Nikon AF-S Micro 40mm 2.8</li>
+                                            <li>Nikon AF-S 50mm 1.8</li>
+                                            <li>Nikon AF 85mm 1.8 D</li>
+                                            <li>Nikon AF-S 18-55mm 3.5-4.5</li>
+                                            <li>Nikon AF-S 55-200mm 4-5.6</li>
+                                            <li>Tamron 17-50mm 2.8</li>
+                                            <li>Tamron 150-600mm 5-6.3 VC</li>
+                                            <li>Sigma 10-20mm 4-5.6</li>
+                                            <li>Sigma 70-300mm Makro</li>
+                                        </ul>
+                                    </div>
+                                    <div class="grid_4">
+                                        <div class="block2_title color1">Beleuchtung</div>
+                                        <ul class="list">
+                                            <li>Triopo TR970N Speedlight</li>
+                                            <li>2 x Neewer VK750 II Speedlite</li>
+                                            <li>Neewer TT660 Speedlight</li>
+                                            <li>2 x Yongnuo 560 II Speedlight</li>
+                                            <li>Yongnuo YN 568 EX Speedlight</li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="clear"></div>
+                            <div class="grid_12">
+                                <section class="bottom_block"></section>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+                </section>
+        </div>    
+    <footer>
+        <?php
+        include './footer.php';
+        ?>
+    </footer>
+</div>
+</body>
+</html>
