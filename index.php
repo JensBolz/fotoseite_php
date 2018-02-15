c90202efe2459e3e5db01535eae134a4289a4c6d
Jens Bolz <jens.bolz@gameforge.com>
2018-02-15 11:12:45 +0100

    now empty

--- index.php
+++ index.php
@@ -21,7 +21,9 @@
                             <div class="menu_block">
                                 <nav class="horizontal-nav full-width horizontalNav-notprocessed">
                                     <ul class="sf-menu">
-                                        <li class="current">Über</li>
+                                        <li class="current">Start</li>
+                                        <li><a href="portfolio_main.php">Portfolio</a></li>
+                                        <li><a href="about.php">Über mich</a></li>
                                         <li><a href="contact.php">Kontakt</a></li>                                        
 
                                     </ul>
@@ -32,9 +34,7 @@
                     </div>
                 </div>
             </div>   
-            <?php
-            include './slider.php';
-            ?>
+            
         </header>
         <div class="main">
             <!--=====================Content======================-->
@@ -54,157 +54,11 @@
             <section>
                 <div class="container">
                     <div class="row">
-                        <div class="grid_12">
-                            <h2>Portfolio</h2>                            
-                            <div class="gallery">
-                                <div class="row">
-                                    <div class="grid_4 gal_item">
-                                        <a href="portfolio_people.php" >
-                                            <img src="images/gallery/thumbs/people/thumb1.jpg" alt="">
-                                            <div class="gal_caption" >Menschen</div>
-                                        </a>
-                                    </div>
-                                    <div class="grid_4 gal_item" >
-                                        <a href="portfolio_classiccars.php">
-                                            <img src="images/home/portfolio/thumbs/classiccars_thumb.jpg" alt="">
-                                            <div class="gal_caption">Classic Cars</div>
-                                        </a>
-                                    </div>
-                                    <div class="grid_4 gal_item" >
-                                        <a href="portfolio_architecture.php">
-                                            <img src="images/home/portfolio/thumbs/arcitecture_thumb.jpg" alt="">
-                                            <div class="gal_caption">Architektur</div>
-                                        </a>
-                                    </div>
-                                    <div class="clear"></div>
-                                    <div class="grid_4 gal_item" >
-                                        <a href="portfolio_animales.php">
-                                            <img src="images/home/portfolio/thumbs/animal_thumb.jpg" alt="">
-                                            <div class="gal_caption">Tiere und Insekten</div>
-                                        </a>
-                                    </div>
-                                    <div class="grid_4 gal_item" >
-                                        <a href="portfolio_landscape.php">
-                                            <img src="images/home/portfolio/thumbs/landscape_thumb.png" alt="">
-                                            <div class="gal_caption">Landschaft</div>
-                                        </a>
-                                    </div>
-                                    <div class="grid_4 gal_item">
-                                        <a href="portfolio_nature.php">
-                                            <img src="images/home/portfolio/thumbs/nature_thumb.jpg" alt="">
-                                            <div class="gal_caption">Natur</div>
-                                        </a>
-                                    </div>
-                                    <div class="clear"></div>
-                                    <div class="grid_4 gal_item">
-                                        <a href="portfolio_cementaries.php">
-                                            <img src="images/home/portfolio/thumbs/cementary_thumb.jpg" alt="">
-                                            <div class="gal_caption">Friedhöfe</div>
-                                        </a>
-                                    </div>                                 
-                                    <div class="grid_4 gal_item">
-                                        <!--    <a href="portfolio_nude.php">
-                                                <img src="images/home/portfolio/thumbs/cementary_thumb.jpg" alt="">
-                                                <div class="gal_caption">Nude Art</div>
-                                            </a>-->
-                                    </div>
-                                    <div class="grid_4 gal_item">
-                                        <a href="portfolio_goth.php">
-                                            <img src="images/home/portfolio/thumbs/goth_thumb.jpg" alt="">
-                                            <div class="gal_caption">Goth und Fetisch</div>
-                                        </a>
-                                    </div>
-                                </div>
-                                <div class="row">
-                                    <div class="clear"></div>
-                                    <div class="grid_4a">
-                                        <h3>Über mich</h3>
-                                        <p>
-                                            Als hauptberuflicher Software- und Webentwickler ist Fotografie für mich ein Ausgleich zum Beruf und eine Art Meditation. Bei meinen eigenen Projekten oder Fototouren fotografiere das was mir Spaß macht und folge nicht einem bestimmten Trend oder irgendwelchen Vorgaben.<br>
-
-                                            Ich probiere auch gerne das ein oder andere aus wie z.B. Blitztechniken mit Aufsteckblitzen oder neue Wege in der Bildbarbeitung.
-                                        </p>
-                                        <p></p>
-                                    </div>
-                                </div> 
-
-                                <div class="row">
-                                    <div class="grid_4a">
-                                        <h3>Fotos im Netz</h3>
-                                        <ul class="list1">
-                                            <li>
-                                                <div class="list_count">1</div>
-                                                <div class="extra_wrapper">
-                                                    <div class="list1_title color2"><a href="https://500px.com/jens79" target="_blank">500px </a></div>Meine Fotos auf 500px
-                                                    <a href="https://500px.com/jens79" target="_blank" class="color1 td_und">More</a>
-                                                </div>
-                                            </li>
-                                            <li>
-                                                <div class="list_count">2</div>
-                                                <div class="extra_wrapper">
-                                                    <div class="list1_title color2"><a href="https://www.flickr.com/photos/jensbolz/" target="_blank">FLICKR</a></div>Meine Fotos auf FLICKR
-                                                    <a href="https://www.flickr.com/photos/jensbolz/" target="_blank" class="color1 td_und">More</a>
-                                                </div>
-                                            </li> 
-                                            <li>
-                                                <div class="list_count">3</div>
-                                                <div class="extra_wrapper">
-                                                    <div class="list1_title color2"><a href="http://www.fotocommunity.de/fotograf/jens-bolz/2147626" target="_blank">fotocommunity</a></div>Meine Fotos in der fotocommunity
-                                                    <a href="http://www.fotocommunity.de/fotograf/jens-bolz/2147626" target="_blank" class="color1 td_und">More</a>
-                                                </div>
-                                            </li>
-                                        </ul>
-                                    </div>
-                                </div>
-
-                                <div class="row">
-                                    <div class="clear"></div>
-                                    <div class="grid_4a">
-                                        <h3 class="mb__0">Equipment</h3>
-                                        <div class="grid_4">
-                                            <div class="block2_title color1">Kameras</div>
-                                            <ul class="list">
-                                                <li>Nikon D3100</li>
-                                                <li>Nikon D7100</li>
-                                                <li>Nikon D7100 mit MB-D15</li>                                                    
-                                            </ul>
-                                        </div>
-                                        <div class="grid_4">
-                                            <div class="block2_title color1">Objektive</div>
-                                            <ul class="list">                                                  
-                                                <li>Nikon AF-S 35mm 1.8 DX</li>
-                                                <li>Nikon AF-S Micro 40mm 2.8</li>
-                                                <li>Nikon AF-S 50mm 1.8</li>
-                                                <li>Nikon AF 85mm 1.8 D</li>
-                                                <li>Nikon AF-S 18-55mm 3.5-4.5</li>
-                                                <li>Nikon AF-S 55-200mm 4-5.6</li>
-                                                <li>Tamron 17-50mm 2.8</li>
-                                                <li>Tamron 150-600mm 5-6.3 VC</li>
-                                                <li>Sigma 10-20mm 4-5.6</li>
-                                                <li>Sigma 70-300mm Makro</li>
-                                            </ul>
-                                        </div>
-                                        <div class="grid_4">
-                                            <div class="block2_title color1">Beleuchtung</div>
-                                            <ul class="list">
-                                                <li>Triopo TR970N Speedlight</li>
-                                                <li>2 x Neewer VK750 II Speedlite</li>
-                                                <li>Neewer TT660 Speedlight</li>
-                                                <li>2 x Yongnuo 560 II Speedlight</li>
-                                                <li>Yongnuo YN 568 EX Speedlight</li>
-                                            </ul>
-                                        </div>
-                                    </div>
-                                </div>
-
-                                <div class="clear"></div>
-                                <div class="grid_12">
-                                    <section class="bottom_block"></section>
-                                </div>
-
-                            </div>
-                        </div>
+                        <div class="grid_12">            
                     </div>
+                </div>
+                <div class="grid_12">
+                    <section class="bottom_block"></section>
                 </div>
             </section>
             <footer>
