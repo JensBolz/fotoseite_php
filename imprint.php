548032a46189dd817a96865125c037640964d72f
Jens Bolz <jens.bolz@gameforge.com>
2018-02-26 10:37:14 +0100

    small design changes to same as the other sites

--- imprint.php
+++ imprint.php
@@ -80,7 +80,7 @@
                     </div>          
                     <div class="headline">Urheberrecht</div>
                     <div class="paragraph">
-                        Die durch die Seitenbetreiber erstellten Inhalte und Werke auf diesen Seiten unterliegen dem deutschen Urheberrecht. Die Vervielfältigung, Bearbeitung, Verbreitung und jede Art der Verwertung
+                        Die durch die Seitenbetreiber erstellten Inhalte und Werke insbesondere die Fotografien, auf diesen Seiten unterliegen dem deutschen Urheberrecht. Die Vervielfältigung, Bearbeitung, Verbreitung und jede Art der Verwertung
                         außerhalb der Grenzen des Urheberrechtes bedürfen der schriftlichen Zustimmung des jeweiligen Autors bzw. Erstellers. Downloads und Kopien dieser Seite sind nicht gestattet. Soweit die Inhalte
                         auf dieser Seite nicht vom Betreiber erstellt wurden, werden die Urheberrechte Dritter beachtet. Insbesondere werden Inhalte Dritter als solche gekennzeichnet. Sollten Sie trotzdem auf eine
                         Urheberrechtsverletzung aufmerksam werden, bitte ich um einen entsprechenden Hinweis. Bei Bekanntwerden von Rechtsverletzungen werde ich derartige Inhalte umgehend entfernen.
